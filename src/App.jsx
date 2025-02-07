9671f269c81a17b46b7d727efd27cc82c4f656d0
Fausto <tozarf@f2:bc:f5:24:8e:68.home>
2025-02-07 14:59:14 -0500

    First Commit, Navbar and Hero sections ready for three js injection

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./sections/Hero";
+import Navbar from "./sections/Navbar";
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto nav-ul">
+      <Navbar />
+      <Hero />
+    </main>
+  );
+};
+
+export default App;
